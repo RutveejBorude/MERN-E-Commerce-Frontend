7d282700a034b8480a4d943a07a2c9281ef16b3c
rutveej <rutveejborude@gmail.com>
2025-03-28 22:39:36 +0530

    Initial Commit

--- src/Components/CopyRight/CopyRight.jsx
+++ src/Components/CopyRight/CopyRight.jsx
@@ -3,16 +3,17 @@ import React from 'react'
 
 const CopyRight = (props) => {
     return (
-        <a href='https://saurabhkhatmode.netlify.app/' target='_blank' rel='noreferrer' >
+        <></>
+        // <a href='https://saurabhkhatmode.netlify.app/' target='_blank' rel='noreferrer' >
 
-            <Typography variant="body1" fontWeight="bold" color="text.secondary" align="center" {...props} style={{ color: '#1976d2',  }}>
-                {' '}
-                {new Date().getFullYear()}
-                {/* {'.'} */}
-                {' © '}
-                Developed By Saurabh Khatmode
-            </Typography>
-        </a>
+        //     <Typography variant="body1" fontWeight="bold" color="text.secondary" align="center" {...props} style={{ color: '#1976d2',  }}>
+        //         {' '}
+        //         {new Date().getFullYear()}
+        //         {/* {'.'} */}
+        //         {' © '}
+        //         Developed By Saurabh Khatmode
+        //     </Typography>
+        // </a>
     )
 }
 
